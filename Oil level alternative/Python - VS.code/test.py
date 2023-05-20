2d1ae7a0cbd3bb55d4bd16e56cce4443f29faeed
Mustafa <69155967+Hunmbal@users.noreply.github.com>
2023-05-20 05:56:02 +0500

    Add files via upload

---
+++ Oil level alternative/Python - VS.code/test.py
@@ -0,0 +1,39 @@
+import serial, os, threading, sys, time
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+#Serial takes these two parameters: serial device and baudrate
+ser = serial.Serial('COM5', 9600)
+current = []
+fig = plt.figure()                                     
+ax = fig.add_subplot(111)
+ax.set_ylabel('Distance')
+
+
+
+def animate(i, current, ser):
+    global max,min
+    if ser.in_waiting:
+        os.system("cls")
+        data = ser.readline()
+        print("Current level: " + data.decode('utf'))
+        input_list = data.split()
+        current.append(float(input_list[1]))
+        
+    
+    ax.clear()  # clear the plot before drawing new data
+    ax.plot(current)
+    ax.set_ylim(100, 0)
+    ax.set_xlim(len(current)-30, len(current)+30)
+    
+  
+
+
+# main -------------------------------------------------------------------------------------------------------------------------
+
+while not ser.in_waiting:
+    pass
+
+   
+ani = animation.FuncAnimation(fig, animate, frames=60, fargs=(current, ser), interval=500) 
+plt.show()
